dbdfa592b449f6cfadd2b3cde46b81eefc4804b4
Vladimir Sakhonchik <Vladimir_Sakhonchik@epam.com>
2022-07-17 23:36:31 +0200

    push loop

--- loops1.sh
+++ loops1.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+#set -x
+foo=$*
 
-# Put your code here
+for (( i=0; i<${#foo}; i++ )); do
+
+    if [[ `echo ${foo:$i:1}` == [A-Z] ]]
+        then
+            lower_str=$(echo "${foo:$i:1}" | tr '[:upper:]' '[:lower:]')
+            revstr=$revstr$lower_str
+   
+    elif [[ `echo ${foo:$i:1}` == [a-z] ]]
+        then
+            uper_str=$(echo "${foo:$i:1}" | tr '[:lower:]' '[:upper:]')
+            revstr=$revstr$uper_str
+    else 
+            other_str=$(echo "${foo:$i:1}")
+            revstr=$revstr$other_str
+    fi
+
+done
+
+echo $revstr | rev
